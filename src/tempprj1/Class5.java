c82da333c73066cfe7f7bb39ab86ce652fee9421
Mohamed <Mohamed@192.168.0.8>
2020-07-30 19:22:47 +0530

    Revert "updated"
    This reverts commit bb53abb051b876bba963017be9e2a5b8b235a63b.

--- src/tempprj1/Class5.java
+++ src/tempprj1/Class5.java
@@ -13,8 +13,7 @@ public class Class5 {
 	 * new update from user1
 	 */
 
-	/*
-	 * Updated by User2
+	/*Updated by User2
 	 * 
 	 * 
 	 * 
@@ -22,5 +21,8 @@ public class Class5 {
 	 * 
 	 * Updated ny user2
 	 */
-
+	
+	
+	
+>>>>>>> branch 'master' of https://github.com/binaryvillage/temp1
 }
